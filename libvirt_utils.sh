55ebc62b71cdce78122f62889c4bc013c9a4fdae
jan-est <jan.tilles@est.tech>
2019-09-06 15:34:50 +0300

    Change ip range, add pool creation, change create_resourse to libvirt_utils, handle VM delete and undefining

--- libvirt_utils.sh
+++ libvirt_utils.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-create_network() { 
-    export NETWORK_NAME="${1}" #should we add the bridge name and create new bridge for each new network to avoid the possible conflicts?
-    envsubst <"./templates/network.xml">"./templates/network_tmp.xml"   
-    if ! [ "$(virsh net-info "${NETWORK_NAME}")" ]; then
+create_network() {
+    export NETWORK_NAME="${1}"
+    envsubst <"./templates/network.xml" >"./templates/network_tmp.xml"
+    if [ -z "$(virsh net-info "${NETWORK_NAME}")" ]; then
         # Network not found, create network NETWORK_NAME
         virsh net-create --file ./templates/network_tmp.xml
         echo Creating network "${NETWORK_NAME}"
@@ -15,7 +15,7 @@ create_network() {
 
 create_pool() {
     # Create pool
-    if ! [ "$(virsh pool-info default)" ]; then
+    if [ -z "$(virsh pool-info default)" ]; then
         # Default pool not found, create pool
         echo Creating default pool
         virsh pool-create --file ./templates/pool.xml
@@ -29,10 +29,10 @@ create_domain() {
     export VOLUME_NAME="${2}"
     export CPU_COUNT="${3}"
     export NETWORK_NAME="${4}"
-    envsubst <"./templates/vm.xml">"./templates/vm_tmp.xml"
-    
+    envsubst <"./templates/vm.xml" >"./templates/vm_tmp.xml"
+
     # Create volume
-    if ! [ "$(virsh vol-info "${VOLUME_NAME}" default)" ]; then
+    if [ -z "$(virsh vol-info "${VOLUME_NAME}" default)" ]; then
         # Create volume
         echo Creating volume "${VOLUME_NAME}"
         virsh vol-create-as default "${VOLUME_NAME}" 8G
@@ -45,7 +45,7 @@ create_domain() {
     fi
 
     # Create virtual machine
-    if ! [ "$(virsh dominfo "${VM_NAME}")" ]; then
+    if [ -z "$(virsh dominfo "${VM_NAME}")" ]; then
         # Create new virtual machine
         echo Creating "${VM_NAME}" virtual machine
         virsh create --file ./templates/vm_tmp.xml
@@ -53,8 +53,35 @@ create_domain() {
         # Old vm with same name found, delete old one and create new
         echo Deleting old virtual machine
         virsh destroy "${VM_NAME}"
+        virsh undefine "${VM_NAME}"
         echo Creating virtual machine
         virsh create --file ./templates/vm_tmp.xml
     fi
     rm -r ./templates/vm_tmp.xml
+}
+
+delete_dev_env() {
+
+    VM_NAME_PREFIX="${1}"
+    NETWORK_NAME="${2}"
+    VOLUME_POSTFIX="${3}"
+    VM_COUNT="${4}"
+    
+    if [ "$(virsh net-info "${NETWORK_NAME}")" ]; then
+        echo Deleting network ${NETWORK_NAME}
+        virsh net-destroy ${NETWORK_NAME}
+        virsh net-undefine ${NETWORK_NAME}
+    fi
+
+    for ((n = 0; n < "${VM_COUNT}"; n++)); do
+        VM_NAME="${VM_NAME_PREFIX}_${n}"
+        VOLUME_NAME="${VM_NAME}_${VOLUME_POSTFIX}"
+        
+        echo Destroying and undefining virtual machine "${VM_NAME}"
+        virsh destroy "${VM_NAME}"
+        virsh undefine "${VM_NAME}"
+        echo Deleting volume "${VOLUME_NAME}"
+        virsh vol-delete "${VOLUME_NAME}" default
+
+    done
 }
--- teardown_dev_env.sh
+++ teardown_dev_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source create_resource.sh
+source libvirt_utils.sh
 
 VM_NAME_PREFIX='dummy_vm'
 NETWORK_NAME='dev01'
@@ -7,15 +7,4 @@ VOLUME_POSTFIX='vol'
 VM_COUNT=2 
 CPU_COUNT=1
 
-if [ "$(virsh net-info "${NETWORK_NAME}")" ]; then
-    virsh net-destroy ${NETWORK_NAME}
-fi
-
-for ((n=0;n<"${VM_COUNT}";n++))
-do
-    VM_NAME="${VM_NAME_PREFIX}_${n}"
-    VOLUME_NAME="${VM_NAME}_${VOLUME_POSTFIX}"
-    virsh destroy "${VM_NAME}"
-    virsh vol-delete "${VOLUME_NAME}" default
-
-done
+delete_dev_env ${VM_NAME_PREFIX} ${NETWORK_NAME} ${VOLUME_POSTFIX} ${VM_COUNT}
